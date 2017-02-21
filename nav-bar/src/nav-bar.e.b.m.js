a0343760ee77f16b70b063b69212f24cca563325
Alexander Elias <alex.steven.elias@gamil.com>
2017-02-21 10:09:20 -0700

    fixed weird size issues, minified css, load time impromevents

--- nav-bar/src/nav-bar.e.b.m.js
+++ nav-bar/src/nav-bar.e.b.m.js
@@ -1,199 +1,31 @@
 /*
+	@preserve
 	title: nav-bar
-	version: 1.1.0
-	author: Alexander Elias
+	version: 1.1.1
+	author: alexander elias
 */
 
-var sStyle = `
-.nav-bar {
-	z-index: 1;
-	width: 100%;
-	text-align: left;
-	position: relative;
-}
-.nav-bar * {
-	margin: 0;
-	padding: 0;
-	transition: all 300ms ease-in-out;
-}
-.nav-bar a {
-	padding: 24px 64px;
-	display: inline-block;
-	vertical-align: middle;
-}
-.nav-bar ul {
-	width: 100%;
-	min-height: 100%;
-	list-style: none;
-	text-align: center;
-}
-.nav-bar li {
-	display: inline-block;
-}
-.nav-bar li:hover, .nav-bar li:focus, .nav-bar li:active, .nav-bar li:visited {
-	background: rgba(0,0,0,0.1);
-}
-.nav-list-main {
-	display: inline-block;
-}
-.nav-list-sub {
-	left: 0;
-    position: absolute;
-	max-height: 1000px; /* unactive transition*/
-}
-.nav-list-sub > li {
-	width: 100%;
-}
-.nav-list-sub > li:nth-child(1) > a {
-	width: 100%;
-	font-size: 0.9rem;
-	text-align: left;
-	font-weight: bold;
-	text-indent: 16px;
-	padding: 24px 0;
-	text-transform: uppercase;
-}
+var sStyle = '.nav-bar a,.nav-bar li,.nav-list-main{display:inline-block}.nav-bar,.nav-bar ul,.nav-list-sub>li{width:100%}.nav-bar{z-index:1;text-align:left;position:relative}.nav-bar *{margin:0;padding:0;transition:all .3s ease-in-out}.nav-bar a{cursor:pointer;padding:24px 64px;vertical-align:middle;min-width:100%;box-sizing:border-box}.nav-bar ul{min-height:100%;list-style:none;text-align:center}.nav-bar li:active,.nav-bar li:focus,.nav-bar li:hover,.nav-bar li:visited{background:rgba(0,0,0,.1)}.nav-list-sub{left:0;position:absolute;max-height:1000px}.nav-list-sub>li:nth-child(1)>a{width:100%;font-size:.9rem;text-align:left;font-weight:700;text-indent:16px;padding:24px 0;text-transform:uppercase}@media screen and (min-width:940px){.nav-tool{height:0}.nav-icon-menu-wrap{transform:scale3d(1,0,1)}.nav-list-sub{transform:translate3d(0,-50%,0) scale3d(1,0,1)}.nav-list-main>li{position:relative}.nav-list-main>li>.nav-list-sub .nav-list-sub{top:0}.nav-list-main>li>.nav-list-sub>li:nth-child(1){display:none}.nav-list-main li.active>.nav-list-sub{transform:translate3d(0,0,0) scale3d(1,1,1)}.nav-bar .unactive{max-height:0}.nav-bar .unactive>li:not(.active)>a{margin:0!important;height:0!important;padding:0!important;touch-action:none;pointer-events:none}}@media screen and (max-width:940px){.nav-bar li,.nav-bar li>a{display:block}.nav-tool{width:100%;height:60px;padding:5px 0;box-sizing:border-box}.nav-icon-menu-wrap{margin:auto 16px;transform:scale3d(1,1,1)}.nav-bar li>a{padding:24px 0}.nav-list-main{position:absolute;transform:translate3d(0,-50%,0) scale3d(1,0,1)}.nav-list-sub{top:0;transform:translate3d(50%,0,0) scale3d(0,1,1)}.nav-bar li.active>.nav-list-sub,.nav-list-main.active{transform:translate3d(0,0,0) scale3d(1,1,1)}}.nav-icon-arrow-down,.nav-icon-arrow-right{width:0;height:0;display:inline-block;vertical-align:middle;transition-delay:.3s}.nav-icon-arrow-down{margin:5px;border-left:.35rem solid transparent;border-right:.35rem solid transparent;border-bottom:.7rem solid currentColor}.nav-icon-arrow-right{margin:5px calc(-5px + .5rem) 5px 5px;border-top:.25rem solid transparent;border-left:.5rem solid currentColor;border-bottom:.25rem solid transparent}li.active>a+ul>li>a>.nav-icon-arrow-down{transform:rotate(-90deg)}li.active>a>.nav-icon-arrow-right{transform:rotate(90deg)}.nav-icon-menu-wrap{height:50px;width:50px;cursor:pointer;position:relative;display:block}.nav-icon-menu{width:100%;height:15%;position:absolute;transform-origin:0 0 0;background:currentColor}.nav-icon-menu:nth-child(1){top:10%}.nav-icon-menu:nth-child(2){top:42.5%}.nav-icon-menu:nth-child(3){top:75%}.nav-bar.active .nav-icon-menu:nth-child(1){height:13%;transform:rotate(45deg) translate3d(13%,-112%,0)}.nav-bar.active .nav-icon-menu:nth-child(2){transform:rotate(45deg) translate3d(-10%,-250%,0)}.nav-bar.active .nav-icon-menu:nth-child(3){transform:rotate(-45deg) translate3d(3%,67%,0)}';
 
-@media screen and (min-width: 940px) {
-	.nav-tool {
-		height: 0;
-	}
-	.nav-icon-menu-wrap  {
-		transform: scale3d(1,0,1);
-	}
-	.nav-list-sub {
-		transform: translate3d(0%,-50%,0) scale3d(1,0,1);
-	}
+var eStyle = document.createElement('style');
+var nStyle = document.createTextNode(sStyle);
 
-	.nav-list-main > li {
-		position: relative;
-	}
-	.nav-list-main > li > .nav-list-sub .nav-list-sub {
-		top: 0;
-	}
-	.nav-list-main > li > .nav-list-sub > li:nth-child(1) {
-		display: none;
-	}
-	.nav-list-main li.active > .nav-list-sub {
-		transform: translate3d(0%,0%,0) scale3d(1,1,1);
-	}
+var arrowDown = document.createElement('div');
+var arrowRight = document.createElement('div');
 
-	.nav-bar .unactive {
-		max-height: 0;
-	}
-	.nav-bar .unactive > li:not(.active) > a {
-		margin:0 !important;
-		height: 0 !important;
-		padding: 0 !important;
-		touch-action: none;
-    	pointer-events: none;
-	}
-}
+var closeA = document.createElement('a');
+var closeLi = document.createElement('li');
+var closeText = document.createTextNode('Close');
 
-@media screen and (max-width: 940px) {
-	.nav-tool {
-		width: 100%;
-		height: 60px;
-	    padding: 5px 0;
-	    box-sizing: border-box;
-	}
-	.nav-icon-menu-wrap {
-		margin: auto 16px;
-		transform: scale3d(1,1,1);
-	}
-	.nav-bar li {
-		display: block;
-	}
-	.nav-bar li > a {
-		display: block;
-		padding: 24px 0;
-	}
-	.nav-list-main {
-		position: absolute;
-		transform: translate3d(0%,-50%,0) scale3d(1,0,1);
-	}
-	.nav-list-sub {
-		top: 0;
-		transform: translate3d(50%,0%,0) scale3d(0,1,1);
-	}
-	.nav-bar li.active > .nav-list-sub, .nav-list-main.active {
-		transform: translate3d(0%,0%,0) scale3d(1,1,1);
-	}
-}
-
-.nav-icon-arrow-down {
-	width: 0;
-	height: 0;
-	margin: 5px;
-	display: inline-block;
-	vertical-align: middle;
-	transition-delay: 300ms;
-	border-left: 0.35rem solid transparent;
-	border-right: 0.35rem solid transparent;
-	border-bottom: 0.7rem solid currentColor;
-}
-.nav-icon-arrow-right {
-	width: 0;
-	height: 0;
-	margin: 5px calc(-5px + 0.5rem) 5px 5px;
-	display: inline-block;
-	vertical-align: middle;
-	transition-delay: 300ms;
-	border-top: 0.25rem solid transparent;
-	border-left: 0.5rem solid currentColor;
-	border-bottom: 0.25rem solid transparent;
-}
-li.active > a+ul > li > a > .nav-icon-arrow-down {
-	transform: rotate(-90deg);
-}
-li.active > a > .nav-icon-arrow-right {
-	transform: rotate(90deg);
-}
-.nav-icon-menu-wrap  {
-	height: 50px;
-	width: 50px;
-	cursor: pointer;
-	position: relative;
-	display: block;
-}
-.nav-icon-menu {
-	width: 100%;
-	height: 15%;
-	position: absolute;
-	transform-origin: 0 0 0;
-	background: currentColor;
-}
-.nav-icon-menu:nth-child(1) {
-	top: 10%;
-}
-.nav-icon-menu:nth-child(2) {
-	top: 42.5%;
-}
-.nav-icon-menu:nth-child(3) {
-	top: 75%;
-}
-.nav-bar.active .nav-icon-menu:nth-child(1) {
-	height: 13%;
-	transform: rotate(45deg) translate3d(13%,-112%,0);
-}
-.nav-bar.active .nav-icon-menu:nth-child(2) {
-	transform: rotate(45deg) translate3d(-10%,-250%,0);
-}
-.nav-bar.active .nav-icon-menu:nth-child(3) {
-	transform: rotate(-45deg) translate3d(3%,67%,0);
-}
-`;
-
-/*
-	main
-*/
+var navTool = document.createElement('div');
+var navIconWrap = document.createElement('div');
+var navIcon = document.createElement('div');
 
 document.addEventListener('DOMContentLoaded', function () {
+
 	/*
 		add stylesheet
 	*/
-	var eStyle = document.createElement('style');
-	var nStyle = document.createTextNode(sStyle);
 	eStyle.appendChild(nStyle);
 	document.head.appendChild(eStyle);
 
@@ -205,20 +37,12 @@ document.addEventListener('DOMContentLoaded', function () {
 	/*
 		arrows
 	*/
-	var arrowDown = document.createElement('div');
 	arrowDown.setAttribute('class', ' nav-icon-arrow-down');
-
-	var arrowRight = document.createElement('div');
 	arrowRight.setAttribute('class', 'nav-icon-arrow-right');
 
 	/*
 		close
 	*/
-	var closeA = document.createElement('a');
-	var closeLi = document.createElement('li');
-	var closeText = document.createTextNode('Close');
-
-	closeA.href = '#!';
 	closeA.appendChild(arrowDown.cloneNode(true));
 	closeA.appendChild(closeText);
 	closeLi.appendChild(closeA);
@@ -226,13 +50,8 @@ document.addEventListener('DOMContentLoaded', function () {
 	/*
 		icon
 	*/
-	var navTool = document.createElement('div');
 	navTool.setAttribute('class', 'nav-tool');
-
-	var navIconWrap = document.createElement('div');
 	navIconWrap.setAttribute('class', 'nav-icon-menu-wrap');
-
-	var navIcon = document.createElement('div');
 	navIcon.setAttribute('class', 'nav-icon-menu');
 
 	for (var c = 0; c < 3; c++) navIconWrap.appendChild(navIcon.cloneNode(true));
@@ -245,9 +64,6 @@ document.addEventListener('DOMContentLoaded', function () {
 	navTool.appendChild(navIconWrap);
 	navBar.insertBefore(navTool, navBar.firstElementChild);
 
-	/*
-		main
-	*/
 	for (var i = 1, l = navListAll.length; i < l; i++) {
 		var closeLiClone = closeLi.cloneNode(true);
 		navListAll[i].insertBefore(closeLiClone, navListAll[i].firstElementChild);
