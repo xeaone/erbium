a367900c25363ad0c13372c0abb55c0f8ed95620
Alexander Elias <alex.steven.elias@gmail.com>
2017-11-26 14:50:43 -0700

    fixed nav menu issues, improved sizes

---
+++ build.js
@@ -0,0 +1,29 @@
+const Muleify = require('muleify');
+const Path = require('path');
+const Util = require('util');
+const Fs = require('fs');
+
+const Read = Util.promisify(Fs.readFile);
+const Write = Util.promisify(Fs.writeFile);
+
+(async function() { try {
+
+	const navBarDistPath = Path.join('nav-bar', 'dist', 'nav-bar.min.js');
+	const navBarJsPath = Path.join(__dirname, 'nav-bar', 'src', 'index.js');
+	const navBarCssPath = Path.join(__dirname, 'nav-bar', 'src', 'index.css');
+
+	let navBarCssFile = await Read(navBarCssPath, 'utf8');
+	let navBarJsFile = await Read(navBarJsPath, 'utf8');
+
+	navBarCssFile = navBarCssFile.replace(/\/\/.*?/g, '');
+	navBarCssFile = navBarCssFile.replace(/\n|\t/g, '');
+	navBarCssFile = navBarCssFile.replace(/\/\*.*?\*\//g, '');
+	navBarJsFile = navBarJsFile.replace('${sStyle}', navBarCssFile);
+
+	Write(navBarDistPath, navBarJsFile);
+
+	Muleify.pack(navBarDistPath, navBarDistPath, {
+		minify: true
+	});
+
+} catch (e) { console.error(e); } }());
--- nav-bar/src/index.js
+++ nav-bar/src/index.js
@@ -1,16 +1,19 @@
 /*
 	@banner
 	title: nav-bar
-	version: 1.1.2
-	author: alexander elias
+	version: 1.1.3
+	license: MPL-2.0
+	author: Alexander Elias
+	email: alex.steven.elias@gmail.com
 */
+var i, l;
 
-var sStyle = '.nav-bar {z-index: 1;width: 100%;display: block;text-align: left;position: relative;}.nav-bar * {margin: 0;padding: 0;transition: all 300ms ease-in-out;}.nav-bar a {cursor: pointer;min-width: 100%;padding: 24px 64px;display: inline-block;vertical-align: middle;box-sizing: border-box;}.nav-bar ul {width: 100%;min-height: 100%;list-style: none;text-align: center;}.nav-bar li {display: inline-block;}.nav-bar li:hover, .nav-bar li:focus, .nav-bar li:active, .nav-bar li:visited {background: rgba(0,0,0,0.1);}.nav-list-main {display: inline-block;}.nav-list-sub {left: 0;    position: absolute;max-height: 1000px;}.nav-list-sub > li {width: 100%;}.nav-list-sub > li:nth-child(1) > a {width: 100%;font-size: 0.9rem;text-align: left;font-weight: bold;text-indent: 16px;padding: 24px 0;text-transform: uppercase;}@media screen and (min-width: 940px) {.nav-tool {height: 0;}.nav-icon-menu-wrap  {transform: scale3d(1,0,1);}.nav-list-sub {transform: translate3d(0%,-50%,0) scale3d(1,0,1);}.nav-list-main > li {position: relative;}.nav-list-main > li > .nav-list-sub .nav-list-sub {top: 0;}.nav-list-main > li > .nav-list-sub > li:nth-child(1) {display: none;}.nav-list-main li.active > .nav-list-sub {transform: translate3d(0%,0%,0) scale3d(1,1,1);}.nav-bar .unactive {max-height: 0;}.nav-bar .unactive > li:not(.active) > a {margin:0;height: 0;padding: 0;touch-action: none;    pointer-events: none;}}@media screen and (max-width: 940px) {.nav-tool {width: 100%;height: 60px;    padding: 5px 0;    box-sizing: border-box;}.nav-icon-menu-wrap {margin: auto 16px;transform: scale3d(1,1,1);}.nav-bar li {display: block;}.nav-bar li > a {display: block;padding: 24px 0;}.nav-list-main {position: absolute;transform: translate3d(0%,-50%,0) scale3d(1,0,1);}.nav-list-sub {top: 0;transform: translate3d(50%,0%,0) scale3d(0,1,1);}.nav-bar li.active > .nav-list-sub, .nav-list-main.active {transform: translate3d(0%,0%,0) scale3d(1,1,1);}}.nav-icon-arrow-down {width: 0;height: 0;margin: 5px;display: inline-block;vertical-align: middle;transition-delay: 300ms;border-left: 0.35rem solid transparent;border-right: 0.35rem solid transparent;border-bottom: 0.7rem solid currentColor;}.nav-icon-arrow-right {width: 0;height: 0;margin: 5px calc(-5px + 0.5rem) 5px 5px;display: inline-block;vertical-align: middle;transition-delay: 300ms;border-top: 0.25rem solid transparent;border-left: 0.5rem solid currentColor;border-bottom: 0.25rem solid transparent;}li.active > a+ul > li > a > .nav-icon-arrow-down {transform: rotate(-90deg);}li.active > a > .nav-icon-arrow-right {transform: rotate(90deg);}.nav-icon-menu-wrap  {width: 50px;height: 50px;display: block;cursor: pointer;position: relative;}.nav-icon-menu {width: 100%;height: 15%;position: absolute;transform-origin: 0 0 0;background: currentColor;}.nav-icon-menu:nth-child(1) {top: 10%;}.nav-icon-menu:nth-child(2) {top: 42.5%;}.nav-icon-menu:nth-child(3) {top: 75%;}.nav-bar.active .nav-icon-menu:nth-child(1) {height: 13%;transform: rotate(45deg) translate3d(13%,-112%,0);}.nav-bar.active .nav-icon-menu:nth-child(2) {transform: rotate(45deg) translate3d(-10%,-250%,0);}.nav-bar.active .nav-icon-menu:nth-child(3) {transform: rotate(-45deg) translate3d(3%,67%,0);}';
+var sStyle = '${sStyle}';
 
 var eStyle = document.createElement('style');
 var nStyle = document.createTextNode(sStyle);
 
-var arrowDown = document.createElement('div');
+var arrowLeft = document.createElement('div');
 var arrowRight = document.createElement('div');
 
 var closeA = document.createElement('a');
@@ -18,11 +21,10 @@ var closeLi = document.createElement('li');
 var closeText = document.createTextNode('Close');
 
 var navTool = document.createElement('div');
-var navIconWrap = document.createElement('div');
 var navIcon = document.createElement('div');
 
 /*
-	add stylesheet
+	stylesheet
 */
 eStyle.appendChild(nStyle);
 document.head.appendChild(eStyle);
@@ -35,35 +37,36 @@ navListMain.setAttribute('class', 'nav-list-main');
 /*
 	arrows
 */
-arrowDown.setAttribute('class', ' nav-icon-arrow-down');
-arrowRight.setAttribute('class', 'nav-icon-arrow-right');
+arrowLeft.setAttribute('class', ' nav-arrow-left');
+arrowRight.setAttribute('class', 'nav-arrow-right');
 
 /*
 	close
 */
-closeA.appendChild(arrowDown.cloneNode(true));
+closeA.appendChild(arrowLeft.cloneNode(true));
 closeA.appendChild(closeText);
 closeLi.appendChild(closeA);
 
 /*
 	icon
 */
-navTool.setAttribute('class', 'nav-tool');
-navIconWrap.setAttribute('class', 'nav-icon-menu-wrap');
-navIcon.setAttribute('class', 'nav-icon-menu');
+navIcon.setAttribute('class', 'nav-icon');
+navIcon.appendChild(document.createElement('div'));
+navIcon.appendChild(document.createElement('div'));
+navIcon.appendChild(document.createElement('div'));
 
-for (var c = 0; c < 3; c++) navIconWrap.appendChild(navIcon.cloneNode(true));
-
-navIconWrap.addEventListener('click', function () {
+navIcon.addEventListener('click', function () {
 	navListMain.classList.toggle('active');
 	navBar.classList.toggle('active');
 });
 
-navTool.appendChild(navIconWrap);
+navTool.appendChild(navIcon);
+navTool.setAttribute('class', 'nav-tool');
 navBar.insertBefore(navTool, navBar.firstElementChild);
 
-for (var i = 1, l = navListAll.length; i < l; i++) {
+for (i = 1, l = navListAll.length; i < l; i++) {
 	var closeLiClone = closeLi.cloneNode(true);
+
 	navListAll[i].insertBefore(closeLiClone, navListAll[i].firstElementChild);
 	navListAll[i].setAttribute('class', 'nav-list-sub');
 
@@ -71,11 +74,13 @@ for (var i = 1, l = navListAll.length; i < l; i++) {
 		close
 	*/
 	closeLiClone.addEventListener('click', function() {
+
 		this.parentNode.parentNode.classList.toggle('active');
 
 		if (this.parentNode.parentNode.parentNode.className === 'nav-list-sub unactive') {
 			this.parentNode.parentNode.parentNode.classList.toggle('unactive');
 		}
+
 	});
 
 	/*
@@ -83,10 +88,13 @@ for (var i = 1, l = navListAll.length; i < l; i++) {
 	*/
 	navListAll[i].previousElementSibling.appendChild(arrowRight.cloneNode(true));
 	navListAll[i].previousElementSibling.addEventListener('click', function() {
+
 		this.parentNode.classList.toggle('active');
 
 		if (this.parentNode.parentNode.className === 'nav-list-sub') {
 			this.parentNode.parentNode.classList.toggle('unactive');
 		}
+
 	});
+
 }
